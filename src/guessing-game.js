8840bf6baee9f9552130f6859b7992b31e6643c1
Ivan Kritsckii <ivan.kritsckii@yandex.ru>
2023-08-29 15:14:30 +0500

    solve test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
+        this.res = (this.max - this.min)/2 + this.min
     }
 
     guess() {
-
+        return this.res
     }
 
     lower() {
-
+        this.max = this.res
+        this.res = Math.ceil((this.res - this.min)/2 + this.min);
+         
     }
 
     greater() {
-
+        this.min = this.res
+        this.res = Math.round((this.max - this.res)/2 + this.res)
     }
 }
 
